759aa5e3d55aedcb80461a1d97522da9b4e63489
sunday1830 <sunday1830_1900@yahoo.co.jp>
2025-08-11 19:51:46 +0900

    Create sw-v9.js
    ver9 calc

---
+++ sw-v9.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = 'bookkeeping-calc-v9';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
